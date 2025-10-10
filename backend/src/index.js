891c51cdbbd7a8ded0ecf4dcb1cebc3e1697f209
MarlinSoe <aungnyeinchan.conceptx@gmail.com>
2025-10-10 13:02:37 +0630

    fix

---
+++ backend/src/index.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import workoutsRoutes from './routes/workoutsRoutes.js'
+import usersRoutes from './routes/usersRoutes.js'
+import { connectDB } from './config/db.js';
+import cors from 'cors'
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001
+
+
+//middleware
+app.use(cors({origin: 'http://localhost:5173'}));
+app.use(express.json());
+
+
+app.use('/api/workouts', workoutsRoutes);
+app.use('/api/user', usersRoutes)
+
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server listening at ${PORT}.`)
+    })
+});
+
